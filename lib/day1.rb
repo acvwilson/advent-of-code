ffa15096d9267431362c96eab34eb57c1d6df8dc
Asa Wilson <awilson@pivotal.io>
2019-12-04 17:20:50 -0500

    Make day 1 great again

--- lib/day1.rb
+++ lib/day1.rb
@@ -1,5 +1,26 @@
 class Day1
-  def calculate(mass)
-    mass / 3 - 2
+  def fuel_requirement_for(mass)
+    fuel_requirement_mass = simple_fuel_requirement(mass)
+    fuel_requirement_total = fuel_requirement_mass
+
+    until fuel_requirement_mass <= 0
+      fuel_requirement_mass = simple_fuel_requirement(fuel_requirement_mass)
+      fuel_requirement_total += fuel_requirement_mass
+    end
+
+    fuel_requirement_total
+  end
+
+  def sum_of_fuel_requirements(masses)
+    masses.sum do |mass|
+      fuel_requirement_for mass
+    end
+  end
+
+private
+  def simple_fuel_requirement(mass)
+    fuel_requirement = mass / 3 - 2
+
+    fuel_requirement < 0 ? 0 : fuel_requirement
   end
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -1,12 +1,21 @@
-require 'day1'
+require "day1"
 
 RSpec.describe Day1 do
   let(:operator) { Day1.new }
+  
+  it "calculates the fuel needed for a module's mass" do
+    expect(operator.fuel_requirement_for(12)).to eq 2
+    expect(operator.fuel_requirement_for(14)).to eq 2
+    expect(operator.fuel_requirement_for(1969)).to eq 966
+    expect(operator.fuel_requirement_for(100756)).to eq 50346
+  end
 
-  it "calculates fuel needs" do
-    expect(operator.calculate(12)).to eq 2
-    expect(operator.calculate(14)).to eq 2
-    expect(operator.calculate(1969)).to eq 654
-    expect(operator.calculate(100756)).to eq 33583
+  it "the minimum fuel requirement is 0" do
+    expect(operator.fuel_requirement_for(4)).to eq 0
+  end
+
+  it "calculate sum of the fuel requirements for the modules" do
+    expect(operator.sum_of_fuel_requirements([12])).to eq 2
+    expect(operator.sum_of_fuel_requirements([12, 24, 600])).to eq 293
   end
 end
